387d76ebc777e0558a2dab3378273f3cda608d9a
yj-044 <yunji8533@naver.com>
2025-05-12 23:18:17 +0900

    ♻️  : 구조 수정

--- src/components/attendance/header/Header.jsx
+++ src/components/attendance/header/Header.jsx
@@ -1,34 +1,19 @@
 import React from "react";
-import styled from "styled-components";
 import Logo from "../../Logo";
 import Time from "./Time";
 import hansung1Logo from "../../../assets/images/hansung1.svg";
 import likelionLogo from "../../../assets/images/like-lion.svg";
-
-const HeaderWrapper = styled.header`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0px 30px;
-    height: 254px;
-`;
-
-// 로고 2개
-const LogoContainer = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 7px;
-`;
+import * as S from "./Header.style";
 
 const Header = () => {
     return (
-      <HeaderWrapper>
-        <LogoContainer>
-          <Logo src={hansung1Logo} width="153px" height="72px" />
-          <Logo src={likelionLogo} width="171px" height="92px" />
-        </LogoContainer>
+        <S.HeaderWrapper>
+        <S.LogoContainer>
+            <Logo src={hansung1Logo} width="153px" height="72px" />
+            <Logo src={likelionLogo} width="171px" height="92px" />
+        </S.LogoContainer>
         <Time />
-      </HeaderWrapper>
+        </S.HeaderWrapper>
     );
 };
 
