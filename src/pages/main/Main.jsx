ad771e0299d7f2d921722e9d39b1f3bd2487c6fa
Suki <tues1205@naver.com>
2025-05-13 21:04:29 +0900

    ✨ : Main.jsx 완성

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,33 +1,37 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Header from "../../components/attendance/header/Header";
 import * as S from "./Main.style";
 
 import CourseInfo from "../../components/attendance/courseInfo/CourseInfo";
 import AttendanceInfo from "../../components/attendance/attendanceInfo/AttendanceInfo";
 import Profile from "../../components/attendance/profile/Profile";
+import Warning from "../../components/attendance/Warning";
+import { useCurrentTime } from "../../hooks/useCurrentTime";
 
 const Main = () => {
-  const [timer, setTimer] = useState(new Date());
+  const timer = useCurrentTime(); // 현재 시간
+  const [showWarning, setShowWarning] = useState(false); // 경고창 유무
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTimer(new Date());
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
+  // 경고창 제어 함수
+  const handleAttendance = (isAllowed) => {
+    if (!isAllowed) {
+      setShowWarning(true);
+    }
+  };
 
   return (
     <>
       <Header timer={timer} />
       <S.MainContainer>
         <S.LeftColumn>
-          <CourseInfo />
+          <CourseInfo timer={timer} onAttendance={handleAttendance} />
           <AttendanceInfo />
         </S.LeftColumn>
         <S.RightColumn>
           <Profile />
         </S.RightColumn>
       </S.MainContainer>
+      {showWarning && <Warning onConfirm={() => setShowWarning(false)} />}
     </>
   );
 };
