6a86f4aca2969e7e4ea35309126eceb33a3832e3
yj-044 <yunji8533@naver.com>
2025-05-13 16:53:43 +0900

    ✨ :  로그인 폼 구현 및 스타일

--- src/pages/login/LoginForm.jsx
+++ src/pages/login/LoginForm.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from "react";
+import Input from "./Input";
+import CheckBox from "./CheckBox";
+import ConfirmButton from "../../components/ConfirmButton";
+import * as S from "./LoginSession.styles";
+
 
 const LoginForm = () => {
-  return <div>폼</div>;
-}
+    return (
+        <S.FormWrapper>
+            <S.LabelRow>
+            <S.LabelText htmlFor="name">이름</S.LabelText>
+            <Input id="name" placeholder="이름을 입력하세요" />
+            </S.LabelRow>
 
-export default LoginForm
+            <S.LabelRow>
+            <S.LabelText htmlFor="studentId">학번</S.LabelText>
+            <Input id="studentId" placeholder="학번을 입력하세요" />
+            </S.LabelRow>
+
+            <S.CheckBoxWrapper>
+            <CheckBox label="자동로그인" />
+            </S.CheckBoxWrapper>
+
+            <S.ButtonWrapper>
+            <ConfirmButton title="LOGIN" width="523px" height="91px" />
+            </S.ButtonWrapper>
+        </S.FormWrapper>
+    );
+};
+
+export default LoginForm;
--- src/pages/login/LoginSession.styles.jsx
+++ src/pages/login/LoginSession.styles.jsx
@@ -1,9 +1,39 @@
 import styled from "styled-components";
 
+// LoginSession
 export const LoginSessionWrapper = styled.div`
     margin-top: 149px;
     display: flex;
     flex-direction: column;
     align-items: center;
     gap: 49px;
+`;
+
+// LoginForm용
+export const FormWrapper = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const LabelRow = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 30px;
+`;
+
+export const LabelText = styled.label`
+  width: 70px;
+  font-size: 30px;
+  font-weight: 400;
+`;
+
+export const CheckBoxWrapper = styled.div`
+  margin-top: 10px;
+`;
+
+export const ButtonWrapper = styled.div`
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
 `;
