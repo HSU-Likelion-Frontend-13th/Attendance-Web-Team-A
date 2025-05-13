07ca36ca554b248e4c4e8aa988dfce6b30ad47b0
Suki <tues1205@naver.com>
2025-05-13 21:36:07 +0900

    ♻️ : 출석 스타일 -> util함수 분리

--- src/utils/checkTime.js
+++ src/utils/checkTime.js
@@ -24,25 +24,3 @@ export const checkAttendanceTime = (course, timer) => {
   // 출석 가능 시간 아님
   return "unavailable";
 };
-
-// ----- 출석버튼 스타일 함수 -----
-export const getAttendanceStyle = (status, clicked, c) => {
-  if (!clicked || status === "unavailable") {
-    return {
-      bgColor: c.boxContentColor,
-      text: "출석하기",
-    };
-  }
-
-  // 출석, 지각, 결석에 따른 배경색과 텍스트
-  const map = {
-    attend: { bgColor: "#014099", text: "출석완료" },
-    late: { bgColor: "#FF8F5C", text: "지\u00A0\u00A0\u00A0각" },
-    absent: { bgColor: "#EB4A3D", text: "결\u00A0\u00A0\u00A0석" },
-  };
-
-  return {
-    ...map[status],
-    textColor: c.white,
-  };
-};
