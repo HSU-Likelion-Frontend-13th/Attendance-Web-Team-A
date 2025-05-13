362e9f20862d8639985c4c94d1593e2e17f87cf4
yj-044 <yunji8533@naver.com>
2025-05-13 14:09:04 +0900

    ✨ : LoginSession 로고 구성

---
+++ src/pages/login/LoginForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const LoginForm = () => {
+  return <div>폼</div>;
+}
+
+export default LoginForm
---
+++ src/pages/login/LoginSession.styles.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const LoginSessionWrapper = styled.div`
+    margin-top: 149px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 49px;
+`;
