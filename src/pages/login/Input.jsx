898dab92c4f76f1ca17c850f276b54d69b01afee
yj-044 <yunji8533@naver.com>
2025-05-13 16:07:53 +0900

    ✨ : 로그인 input창 구현

---
+++ src/pages/login/Input.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+const InputField = styled.input`
+    width: 464px;
+    height: 74px;
+    padding: 0 20px;
+    font-size: 30px;
+    font-weight: 400;
+    border: none;
+    border-radius: 50px;
+    background-color: #d9d9d9;
+    outline: none;
+
+    &::placeholder {
+        color: #9e9e9e;
+        font-size: 30px;
+        font-weight: 400;
+    }
+`;
+
+const Input = ({ type = "text", placeholder, value, onChange, id }) => {
+    return (
+        <InputField
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        />
+    );
+};
+
+export default Input;
