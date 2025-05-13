9d80dc2276ef96480f7b86f698f01b87382793d7
yj-044 <yunji8533@naver.com>
2025-05-14 00:31:56 +0900

    ✨ : 라우터 적용 (Login, Main 연결)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-// import Main from "./pages/main/Main";
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Main from "./pages/main/Main";
 import Login from "./pages/login/Login";
 import GlobalStyle from "./styles/globalStyles";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/main",
+    element: <Main />,
+  },
+]);
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      {/* <Main /> */}
-      <Login />
+      <RouterProvider router={router} />
     </>
   );
 }
+
 export default App;
