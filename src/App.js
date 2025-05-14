d9425c52fbbeb5837f70183c84b721fe1db7300e
yj-044 <yunji8533@naver.com>
2025-05-14 14:15:57 +0900

    ✨: 라우터 구조 변경

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from "react";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Main from "./pages/main/Main";
-import Login from "./pages/login/Login";
+import { Outlet } from "react-router-dom";
 import GlobalStyle from "./styles/globalStyles";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />,
-  },
-  {
-    path: "/main",
-    element: <Main />,
-  },
-]);
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <RouterProvider router={router} />
+      <Outlet />
     </>
   );
 }
